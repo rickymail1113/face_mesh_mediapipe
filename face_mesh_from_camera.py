85ecff4c591de5601f0f66feb4010d48bb8f4bc0
rickymail1113 <rickymail1113@gmail.com>
2023-08-29 11:16:21 +0800

    修改物件名稱

--- face_mesh_from_camera.py
+++ face_mesh_from_camera.py
@@ -1,10 +1,10 @@
 import cv2
 import mediapipe as mp
 
-mp_drawng = mp.solutions.drawing_utils
+mp_drawing = mp.solutions.drawing_utils
 mp_drawing_style = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
-drawing_spec = mp_drawng.DrawingSpec(thickness=1, circle_radius=1)
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
 
 with mp_face_mesh.FaceMesh(max_num_faces=5, refine_landmarks=True, min_detection_confidence=0.5,
@@ -27,17 +27,17 @@ min_tracking_confidence=0.5) as face_mesh:
                     print(point)
 
                 # 臉部 網格
-                mp_drawng.draw_landmarks(image=image, landmark_list=i, connections=mp_face_mesh.FACEMESH_TESSELATION,
+                mp_drawing.draw_landmarks(image=image, landmark_list=i, connections=mp_face_mesh.FACEMESH_TESSELATION,
                                          landmark_drawing_spec=None,
                                          connection_drawing_spec=mp_drawing_style.get_default_face_mesh_tesselation_style())
 
                 # 臉、眉、嘴 輪廓
-                mp_drawng.draw_landmarks(image=image, landmark_list=i, connections=mp_face_mesh.FACEMESH_CONTOURS,
+                mp_drawing.draw_landmarks(image=image, landmark_list=i, connections=mp_face_mesh.FACEMESH_CONTOURS,
                                          landmark_drawing_spec=None,
                                          connection_drawing_spec=mp_drawing_style.get_default_face_mesh_tesselation_style())
 
                 # 瞳孔
-                mp_drawng.draw_landmarks(image=image, landmark_list=i, connections=mp_face_mesh.FACEMESH_IRISES,
+                mp_drawing.draw_landmarks(image=image, landmark_list=i, connections=mp_face_mesh.FACEMESH_IRISES,
                                          landmark_drawing_spec=None,
                                          connection_drawing_spec=mp_drawing_style.get_default_face_mesh_tesselation_style())
 
